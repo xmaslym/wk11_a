cea131afed365e6c28d15f9027195a5fe4789f0d
xmaslym <141024294+xmaslym@users.noreply.github.com>
2023-10-24 02:30:16 +0800

    commit

--- unit_test.py
+++ unit_test.py
@@ -3,8 +3,16 @@ from hello_world import HelloWorld
 
 class TestHelloWorld(unittest.TestCase):
     def test_helloWorld(self):
-        a = HelloWorld("Hello World!")
-        self.assertEqual(a.msg, "Hello World!")
+        a1 = HelloWorld("Hello World!")
+        self.assertEqual(a1.msg, "Hello World!")
+    def test_setMsg(self):
+        a2 = HelloWorld()
+        a2.setMsg("Hello World Everyone!")  
+        self.assertEqual(a2.msg, "Hello World Everyone!")
+    def test_featureX(self):
+        a3 = HelloWorld()
+        a3.featureX("HW Special!")
+        self.assertEqual(a3.msg, "HW Special! featureX")
 
 if __name__ == '__main__':
     unittest.main()
